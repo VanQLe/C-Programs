030d090b6d2e973606340aba50ef835b8b0bf154
VanQLe <van.le@pridetek.com>
2015-04-04 12:16:34 -0400

    No changes

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WORLD");
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Public\TestFolder\WriteLines2.txt");
+            //no changes
+        }
+
+    }
+}
