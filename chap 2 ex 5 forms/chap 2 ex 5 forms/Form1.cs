cbe78fca3583f0f493817b8d808ae73e62d9b811
VanQLe <van.le@pridetek.com>
2015-04-07 19:36:33 -0400

    finish

---
+++ chap 2 ex 5 forms/chap 2 ex 5 forms/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chap_2_ex_5_forms {
+   public partial class Form1 : Form {
+      public Form1() {
+         InitializeComponent();
+      }
+
+      private void button1_Click(object sender, EventArgs e) {
+         if(checkBox1.Checked != false) 
+            if(label1.Text == "Right") {
+               label1.TextAlign = ContentAlignment.MiddleLeft;
+               label1.Text = "Left";
+            
+            }
+            else{
+               label1.Text = "Right";
+               label1.TextAlign = ContentAlignment.MiddleRight;
+            }
+         else {
+            label1.Text = "Text changing is disabled";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+         }
+           
+      }
+   }
+}
---
+++ chap 2 ex 5 forms/chap 2 ex 5 forms/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chap_2_ex_5_forms {
+   static class Program {
+      /// <summary>
+      /// The main entry point for the application.
+      /// </summary>
+      [STAThread]
+      static void Main() {
+         Application.EnableVisualStyles();
+         Application.SetCompatibleTextRenderingDefault(false);
+         Application.Run(new Form1());
+      }
+   }
+}
