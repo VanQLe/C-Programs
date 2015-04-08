7ab3216488fc78d49bd083deb46cbdac129621e1
VanQLe <van.le@pridetek.com>
2015-04-08 15:28:42 -0400

    Finish

---
+++ chp3 ex1 guysclass/chp3 ex1 guysclass/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chp3_ex1_guysclass {
+   public partial class Form1 : Form {
+     Guy joe;
+     Guy bob;
+     int bank;
+      public Form1() {
+        InitializeComponent();
+        joe = new Guy();
+        joe.name = "joe";
+        joe.cash = 100;
+        bob = new Guy();
+        bob.name = "Bob";
+        bob.cash = 100;
+        bank = 100;
+        updateForm();
+      }//end constructor
+
+      public void updateForm() {
+        joeLable1.Text = joe.name + " has $" + joe.cash;
+        bobLable1.Text = joe.name + " has $" + joe.cash;
+        bankLable1.Text = joe.name + " has $" + joe.cash; 
+      }
+
+      private void button1_Click(object sender, EventArgs e) {
+
+      }
+   }
+}
---
+++ chp3 ex1 guysclass/chp3 ex1 guysclass/Guy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chp3_ex1_guysclass {
+	class Guy {
+		public int cash;
+    public string name = "";
+		public int giveCash(int amount) {
+				if(cash <= amount && cash > 0){
+					cash -= amount;
+					return amount;
+				}//end if
+				else {
+					MessageBox.Show("I don't have enough cash to give you" + amount + name + "says...");
+					return 0;
+				}
+		}//end method
+
+	}//end class
+}//end namespace
