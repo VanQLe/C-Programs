9cb451bbccf55b8c0303b72df503b4ddb39a6306
VanQLe <van.le@pridetek.com>
2015-04-07 20:15:05 -0400

    Final

---
+++ chap 2 ex 5 forms/chap 2 ex 5 forms/AnotherClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chap_2_ex_5_forms {
+   class AnotherClass {
+      public static void Main() {
+         MessageBox.Show("POW!");
+      }
+   }
+}
--- chap2 ex5/chap2 ex5/Program.cs
+++ chap2 ex5/chap2 ex5/Program.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace chap_2_ex_5_forms {
+namespace chap2_ex5 {
    static class Program {
       /// <summary>
       /// The main entry point for the application.
       /// </summary>
       [STAThread]
-      static void NOTMain() {
+      static void Main() {
          Application.EnableVisualStyles();
          Application.SetCompatibleTextRenderingDefault(false);
          Application.Run(new Form1());
